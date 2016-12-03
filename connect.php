c10b079534d80d326cbf4faa9f6445f91ec5cb1b
boski-mehta <boskim.3ginfio@gmail.com>
2016-12-03 12:01:39 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,3 +1,25 @@
+<?php 
+session_start();
+require('buffer.php'); 
+$client_id = '583ebbae0d684f7d4c754055';
+	$client_secret = '674004b6c073f97c7159d7ac7ec4b227';
+	$callback_url = 'https://buffer-share.herokuapp.com/';
+
+$buffer = new BufferApp($client_id, $client_secret, $callback_url);
+		
+	if (!$buffer->ok) {
+		echo '<a href="' . $buffer->get_login_url() . '">Connect to Buffer!</a>';
+	} else {
+		$profiles = $buffer->go('/profiles');
+			
+		if (is_array($profiles)) {
+			foreach ($profiles as $profile) {
+				$buffer->go('/updates/create', array('text' => 'My first status update from bufferapp-php worked!', 'profile_ids[]' => $profile->id));
+			}
+		}
+	}
+
+?>
 <!-- HTML Content for Product  START      -->
 
 <div class="product-card-clearfix">
