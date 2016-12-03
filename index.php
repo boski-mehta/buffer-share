99d527a9f80f0a826f08282ca511d297145326f2
boski-mehta <boskim.3ginfio@gmail.com>
2016-12-03 15:35:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -16,9 +16,10 @@ if((isset($_REQUEST['shop'])) && (isset($_REQUEST['code'])) && $_REQUEST['shop']
 	$_SESSION['code']=$_REQUEST['code'];
 }
 $shop_url=$_REQUEST['shop'];
-echo $select_store = pg_query($dbconn4,"SELECT access_token FROM store_info WHERE store_url = $shop_url"); 
+echo $select_store = pg_query($dbconn4,"SELECT access_token FROM store_info WHERE store_url = '$shop_url'"); 
 //$select_store1 = pg_query($dbconn4, $select_store);
 //check if the store exists
+exit();
 if($select_store->pg_num_rows > 0){
 $data = pg_fetch_assoc($select_store);
 echo $access_token= $data['access_token'];
