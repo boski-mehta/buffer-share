1e03f2b10b018d1617ee2655970d4a155ac5f282
boski-mehta <boskim.3ginfio@gmail.com>
2016-12-03 13:58:59 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php 
-$conn_string = "host=ec2-54-221-244-62.compute-1.amazonaws.com port=5432 dbname=dc1l04vbvokmlr user=jihnymieqgdkut password=rhTxZwKWuAKdvqaU21oziua-MX";
+$conn_string = "postgres://qpevunpbkpaeje:4gCdE7avETx_BP2BPg0V2a4qIz@ec2-54-221-245-174.compute-1.amazonaws.com:5432/dckvl1dlptjamr";
 $dbconn4 = pg_connect($conn_string);
 //connect to a database named "dcdhmp2hbn5ol8" on the host "ec2-174-129-223-35.compute-1.amazonaws.com" with a username and password
 if(!$dbconn4){ 
